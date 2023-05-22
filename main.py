5c009457e126c0e3062307781aaeed4cb83a2a3c
V.T.TuongDi <vothituongduy20@gmail.com>
2023-05-22 18:13:18 +0700

    add module view detail

--- main.py
+++ main.py
@@ -39,24 +39,7 @@ class Detail(qtw.QDialog):
         pageName = "details" + str(page) + ".ui"
         uic.loadUi(pageName, self)
         self.btn_close.clicked.connect(lambda:self.close())
-        
-class Detail1(qtw.QDialog):
-    def __init__(self):
-        super(Detail1, self).__init__()
-        uic.loadUi("details1.ui", self)
-        self.btn_close1.clicked.connect(lambda:self.close())
-
-class Detail2(qtw.QDialog):
-    def __init__(self):
-        super(Detail2, self).__init__()
-        uic.loadUi("details2.ui", self)
-        self.btn_close2.clicked.connect(lambda:self.close())
-
-class Detail3(qtw.QDialog):
-    def __init__(self):
-        super(Detail3, self).__init__()
-        uic.loadUi("details3.ui", self)
-        self.btn_close3.clicked.connect(lambda:self.close())
+    
         
                             
 if __name__ == '__main__':
