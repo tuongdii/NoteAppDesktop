9133295b3f2b38983bed0f820ac51431954581e5
V.T.TuongDi <vothituongduy20@gmail.com>
2023-05-22 15:23:17 +0700

    add page transition

--- main.py
+++ main.py
@@ -1,37 +1,43 @@
 from PyQt6 import QtWidgets as qtw
 from PyQt6 import uic
 import sys 
+import re
 
+page = 1
 class NoteApp(qtw.QDialog):
     def __init__(self):
         super(NoteApp, self).__init__()
         uic.loadUi("noteApps.ui", self)
-        self.btn_edit.clicked.connect(self.edit)
-        self.btn_edit1.clicked.connect(self.edit1)
-        self.btn_edit2.clicked.connect(self.edit2)
-        self.btn_edit3.clicked.connect(self.edit3)
+        
+        self.btn_edit1.clicked.connect(self.updatePage)
+        self.btn_edit1.clicked.connect(self.edit)
+        
+        self.btn_edit2.clicked.connect(self.updatePage)
+        self.btn_edit2.clicked.connect(self.edit)
+        
+        self.btn_edit3.clicked.connect(self.updatePage)
+        self.btn_edit3.clicked.connect(self.edit)
+        
+        self.btn_edit4.clicked.connect(self.updatePage)
+        self.btn_edit4.clicked.connect(self.edit)
 
     
     def edit(self):
         self.detail = Detail()
         self.detail.show()
         
-    def edit1(self):
-        self.detail1 = Detail1()
-        self.detail1.show()
-        
-    def edit2(self):
-        self.detail2 = Detail2()
-        self.detail2.show()
-        
-    def edit3(self):
-        self.detail3 = Detail3()
-        self.detail3.show()
+    def updatePage(self):
+        global page
+        button_name = self.sender().objectName()
+        page_number = re.search(r'\d+', button_name)
+        if page_number:
+            page = int(page_number.group())
     
 class Detail(qtw.QDialog):
     def __init__(self):
         super(Detail, self).__init__()
-        uic.loadUi("details.ui", self)
+        pageName = "details" + str(page) + ".ui"
+        uic.loadUi(pageName, self)
         self.btn_close.clicked.connect(lambda:self.close())
         
 class Detail1(qtw.QDialog):
